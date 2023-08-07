ca0a9dc6a45e3ba308526d22bd4fb05c89b76909
Phiphungg <phiphung0102@gmail.com>
2023-08-07 21:59:10 +0700

    upload source code

---
+++ app/src/main/java/com/blogspot/atifsoftwares/sharedpreferences/MainActivity.java
@@ -0,0 +1,76 @@
+package com.blogspot.atifsoftwares.sharedpreferences;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText mNameEt, mAgeEt;
+    Button mSaveBtn, mInfoBtn, mClearBtn;
+    Switch mSwitch;
+    TextView mInfoTv, outputName, outputAge;
+
+    SharedPreferences sharedPreferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mNameEt = findViewById(R.id.nameEt);
+        mAgeEt = findViewById(R.id.ageEt);
+        mSaveBtn = findViewById(R.id.saveBtn);
+        mInfoBtn = findViewById(R.id.showBtn);
+        mSwitch = findViewById(R.id.switchh);
+        mInfoTv = findViewById(R.id.infoTv);
+        mClearBtn = findViewById(R.id.clearBtn);
+        outputName = findViewById(R.id.nameEt);
+        outputAge = findViewById(R.id.ageEt);
+
+                sharedPreferences = getSharedPreferences("SP_NAME", MODE_PRIVATE);
+
+        mSaveBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                String name = mNameEt.getText().toString();
+                int age = Integer.parseInt(mAgeEt.getText().toString().trim());
+                boolean experience = mSwitch.isChecked();
+
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("NAME", name);
+                editor.putInt("AGE", age);
+                editor.putBoolean("EXPERIENCE", experience);
+                editor.apply();
+            }
+        });
+
+        mInfoBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                String name = sharedPreferences.getString("NAME","");
+                int age = sharedPreferences.getInt("AGE",0);
+                boolean experience = sharedPreferences.getBoolean("EXPERIENCE",false);
+
+                mInfoTv.setText("Name: "+name
+                        +"\nAge: "+age
+                        +"\nExperience: "+experience);
+            }
+        });
+
+        mClearBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.clear().apply();
+            }
+        });
+    }
+}
